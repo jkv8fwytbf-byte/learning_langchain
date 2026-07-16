8ece53fea9119d5d653bede987e82262a36b9a6f
siddharth <siddharth.muramkar1@gmail.com>
2026-07-16 09:09:10 +0530

    2

---
+++ working_llm.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage, SystemMessage
+
+load_dotenv()
+
