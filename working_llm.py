207afadb05d07f1a468cc876ad555bc8d6d4482a
siddharth <siddharth.muramkar1@gmail.com>
2026-07-19 08:12:23 +0530

    working with llms

--- working_llm.py
+++ working_llm.py
@@ -3,6 +3,34 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
+
 
 load_dotenv()
 
+
+def demo_message():
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+    # using message objects (more control over roles)
+    messages = [
+        SystemMessage(content="You are a pirate. Always answer like a pirate."),
+        HumanMessage(content="What's the weather like today?"),
+    ]
+    # print("Using message objects:")
+    # print(f"Messages: {messages[0]} | {messages[1]}")
+
+    response = model.invoke(messages)
+    print(f"Response from the Pirate: {response.content}")
+
+    # Multi-turn conversation using message objects
+    messages.append(response)  # add model's response to the conversation
+    messages.append(HumanMessage(content="What about tomorrow?"))
+
+    print("\nMulti-turn conversation:")
+    response = model.invoke(messages)
+    print(f"Follow-up response from the Pirate: {response.content}")
+
+
+if __name__ == "__main__":
+    demo_message()
